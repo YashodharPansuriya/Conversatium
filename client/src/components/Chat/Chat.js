2f20b2b7069403d20d5d463cee11db713487972b
YashodharPansuriya <yashodhar.pansuriya@gmail.com>
2022-12-29 00:07:18 +0100

    connect the client to server side

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,4 +1,26 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+let socket;
+
+const Chat = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() =>{
+        const { name, room } = queryString.parse(location.search);
+        socket = io(ENDPOINT);
+
+        setRoom(room);
+        setName(name)
+
+        console.log(socket);
+    })
+}
 
 const chat = () => {
     return (
