325c10273f234bf618ff7f337bb37c0669730e85
YashodharPansuriya <yashodhar.pansuriya@gmail.com>
2023-01-03 09:21:17 +0100

    deployed the server-side in Heroku and client-side in Netlifd

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -18,7 +18,7 @@ const Chat = ({ location }) => {
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
 
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://conversatium.herokuapp.com/';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
